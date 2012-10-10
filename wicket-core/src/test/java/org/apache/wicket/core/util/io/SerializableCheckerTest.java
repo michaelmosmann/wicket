f977698af166e98fe6ac08606411da8d43af1138
Michael Mosmann <michael@mosmann.de>
2012-10-10 21:32:42 +0200

    start with a test

---
+++ wicket-core/src/test/java/org/apache/wicket/core/util/io/SerializableCheckerTest.java
@@ -0,0 +1,118 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.wicket.core.util.io;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import junit.framework.Assert;
+
+import org.apache.wicket.core.util.io.SerializableChecker.WicketNotSerializableException;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.junit.Test;
+
+/**
+ * Test some standard cases which this class should detect
+ * 
+ * @author mosmann
+ */
+public class SerializableCheckerTest implements Serializable
+{
+	/**
+	 * check a not serializable bean which should throw an exception
+	 * 
+	 * @throws IOException
+	 */
+	@Test(expected = WicketNotSerializableException.class)
+	public void notSerializableBean() throws IOException
+	{
+		new SerializableChecker(null).writeObject(new SimpleBean());
+	}
+
+	@Test(expected = WicketNotSerializableException.class)
+	public void beanIsNotSerializable() throws IOException
+	{
+		checkWith(new SimpleBean());
+	}
+
+	@Test
+	public void beanIsSerializable() throws IOException
+	{
+		checkWith(new SerialBean());
+	}
+
+	@Test
+	// (expected = WicketNotSerializableException.class)
+	public void customTypeIsNotSerializable() throws IOException
+	{
+		checkWith(new CouldBeADomainType());
+	}
+
+	@Test
+	// (expected = WicketNotSerializableException.class)
+	public void notDetachedLDM() throws IOException
+	{
+		IModel<String> model = new LoadableDetachableModel<String>()
+		{
+			@Override
+			protected String load()
+			{
+				return "dont care about the value here";
+			}
+		};
+		Assert.assertNotNull(model.getObject());
+		checkWith(new CouldBeAPanel("panel", model));
+	}
+
+
+	private void checkWith(Object object) throws IOException
+	{
+		new SerializableChecker(null).writeObject(object);
+	}
+
+	public static class SimpleBean
+	{
+
+	}
+
+
+	static class SerialBean implements Serializable
+	{
+		String name = "killer";
+	}
+
+	static class CouldBeADomainType implements Serializable, DoNotSerializeMe
+	{
+
+	}
+
+	static class CouldBeAPanel implements Serializable
+	{
+		final Object someValue;
+
+		public CouldBeAPanel(String id, Object someValue)
+		{
+			this.someValue = someValue;
+		}
+	}
+
+	static interface DoNotSerializeMe
+	{
+
+	}
+}
